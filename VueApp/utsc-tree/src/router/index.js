de37ffd0be8da5bc35b87a62218304bef4720b1c
Justin Lyn <justin.lyn@mail.utoronto.ca>
2020-02-16 16:37:28 -0500

    Worked on the admin page (added in settings, analytics sections, user table)

--- VueApp/utsc-tree/src/router/index.js
+++ VueApp/utsc-tree/src/router/index.js
@@ -35,12 +35,12 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Information.vue')
   },
   {
-    path: '/adminpage',
-    name: 'adminpage',
+    path: '/admin',
+    name: 'admin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AdminPage.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue')
   }
 ]
 
---
+++ VueApp/utsc-tree/src/views/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Admin Panel</h1>
+    <AdminPage></AdminPage>
+  </div>
+
+</template>
+
+<script>
+import AdminPage from '@/components/AdminPage.vue'
+export default {
+  name: 'Admin',
+  components: {
+    AdminPage
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    padding-top: 15px;
+    padding-left: 20px;
+  }
+
+</style>
